1cfd6e2aae994b2dfb6c1248f17b281b3c1b68b3
BVerschaete <bastien.verschaete@gmail.com>
2016-03-15 20:03:54 +0100

    tweenjs/easejs example

--- js/index.js
+++ js/index.js
@@ -2,8 +2,37 @@
  * Created by Bastien on 3/12/2015.
  */
 
-function setup() {
+var disqus_config = function () {
+    this.page.url = "http://bverschaete.github.io/";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = ""; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
 
+function setup() {
+    var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("Crimson").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    createjs.Tween.get(circle, {loop: true})
+        .to({x: 400}, 1000, createjs.Ease.getPowInOut(4))
+        .to({alpha: 0, y: 75}, 500, createjs.Ease.getPowInOut(2))
+        .to({alpha: 0, y: 125}, 100)
+        .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
+        .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", stage);
+    useDisqus();
 }
+
+function useDisqus() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+    var d = document, s = d.createElement('script');
+
+    s.src = '//webdesigngame.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+}
+
 
 window.addEventListener("load", setup);
