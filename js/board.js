22f0a9c1f442056beec9b4b34d904a5071cfca67
TellMeDoYouBleed <crossfireke@hotmail.com>
2016-03-16 21:23:01 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- js/board.js
+++ js/board.js
@@ -14,3 +14,18 @@ var board = [
     [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
 ]];
 
+var posX = 0;
+var posY = 1;
+var direction = 1; // top = 0, right = 1, down = 2, left = 3
+
+var move = function() {
+    if(direction != 2 && board[posY-1][posX] != null && board[posY-1][posX] == 1 ){
+        posY -= 1;
+    }else if(direction != 3 && board[posY][posX+1] != null && board[posY][posX+1] == 1 ){
+        posX += 1;
+    }else if(direction != 0 && board[posY+1][posX] != null && board[posY+1][posX] == 1 ){
+        posY += 1;
+    }else if(direction != 1 && board[posY][posX-1] != null && board[posY][posX-1] == 1 ){
+        posX -= 1;
+    }
+};
