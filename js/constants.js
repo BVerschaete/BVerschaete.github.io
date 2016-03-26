8ecb20b36352fe1ee8943438ee55cda016bea2ab
BVerschaete <bastien.verschaete@gmail.com>
2016-03-26 22:18:06 +0100

    added single water lines

--- js/constants.js
+++ js/constants.js
@@ -17,55 +17,70 @@ var attackerCodes = {
 //all different tiles, add to tiles object afterwards
 var grass = new Image();
 grass.src = "img/tiles/0.png";
-var water = new Image();
-water.src = "img/tiles/1.png";
 var horizontal = new Image();
-horizontal.src = "img/tiles/2.png";
+horizontal.src = "img/tiles/1.png";
 var vertical = new Image();
-vertical.src = "img/tiles/3.png";
+vertical.src = "img/tiles/2.png";
 var cornerLeftDown = new Image();
-cornerLeftDown.src = "img/tiles/4.png";
+cornerLeftDown.src = "img/tiles/3.png";
 var cornerLeftUp = new Image();
-cornerLeftUp.src = "img/tiles/5.png";
+cornerLeftUp.src = "img/tiles/4.png";
 var cornerRightUp = new Image();
-cornerRightUp.src = "img/tiles/6.png";
+cornerRightUp.src = "img/tiles/5.png";
 var cornerRightDown = new Image();
-cornerRightDown.src = "img/tiles/7.png";
+cornerRightDown.src = "img/tiles/6.png";
 var waterCornerRightUp = new Image();
-waterCornerRightUp.src = "img/tiles/8.png";
+waterCornerRightUp.src = "img/tiles/7.png";
 var waterRight = new Image();
-waterRight.src = "img/tiles/9.png";
+waterRight.src = "img/tiles/8.png";
 var waterCornerRightDown = new Image();
-waterCornerRightDown.src = "img/tiles/10.png";
+waterCornerRightDown.src = "img/tiles/9.png";
 var waterDown = new Image();
-waterDown.src = "img/tiles/11.png";
+waterDown.src = "img/tiles/10.png";
 var waterCornerLeftDown = new Image();
-waterCornerLeftDown.src = "img/tiles/12.png";
+waterCornerLeftDown.src = "img/tiles/11.png";
 var waterLeft = new Image();
-waterLeft.src = "img/tiles/13.png";
+waterLeft.src = "img/tiles/12.png";
 var waterCornerLeftTop = new Image();
-waterCornerLeftTop.src = "img/tiles/14.png";
+waterCornerLeftTop.src = "img/tiles/13.png";
 var waterUp = new Image();
-waterUp.src = "img/tiles/15.png";
+waterUp.src = "img/tiles/14.png";
 var waterMiddle = new Image();
-waterMiddle.src = "img/tiles/16.png";
+waterMiddle.src = "img/tiles/15.png";
+var waterSingleVertical = new Image();
+waterSingleVertical.src = "img/tiles/16.png";
+var waterSingleHorizontal = new Image();
+waterSingleHorizontal.src = "img/tiles/17.png";
+var waterSingleBottom = new Image();
+waterSingleBottom.src = "img/tiles/18.png";
+var waterSingleTop = new Image();
+waterSingleTop.src = "img/tiles/19.png";
+var waterSingleRight = new Image();
+waterSingleRight.src = "img/tiles/20.png";
+var waterSingleLeft = new Image();
+waterSingleLeft.src = "img/tiles/21.png";
 
 var tiles = {
     0: grass,
-    1: water,
-    2: horizontal,
-    3: vertical,
-    4: cornerLeftDown,
-    5: cornerLeftUp,
-    6: cornerRightUp,
-    7: cornerRightDown,
-    8: waterCornerRightUp,
-    9: waterRight,
-    10: waterCornerRightDown,
-    11: waterDown,
-    12: waterCornerLeftDown,
-    13: waterLeft,
-    14: waterCornerLeftTop,
-    15: waterUp,
-    16: waterMiddle
+    1: horizontal,
+    2: vertical,
+    3: cornerLeftDown,
+    4: cornerLeftUp,
+    5: cornerRightUp,
+    6: cornerRightDown,
+    7: waterCornerRightUp,
+    8: waterRight,
+    9: waterCornerRightDown,
+    10: waterDown,
+    11: waterCornerLeftDown,
+    12: waterLeft,
+    13: waterCornerLeftTop,
+    14: waterUp,
+    15: waterMiddle,
+    16: waterSingleVertical,
+    17: waterSingleHorizontal,
+    18: waterSingleBottom,
+    19: waterSingleTop,
+    20: waterSingleRight,
+    21: waterSingleLeft
 };
