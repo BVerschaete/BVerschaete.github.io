26f141a95f06c1e1a13bd7d02ae4caa2d1c298ab
BVerschaete <bastien.verschaete@gmail.com>
2016-04-09 21:52:27 +0200

    moved script tag from index

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@
 
 //zorgt ervoor dat spelernaam wordt doorgegeven aan gamepagina
 function setup() {
+    //voor de cookie pop up
+    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":null,"theme":"dark-floating"};
+    
     scroll();
     var start = $('#startGame');
     var input = $('#usernameInput');
