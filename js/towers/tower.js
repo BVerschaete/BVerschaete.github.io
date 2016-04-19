e3ae6b9fc54dff6982e52c19b912906f8fe305e7
BVerschaete <bastien.verschaete@gmail.com>
2016-04-19 23:20:23 +0200

    changed color on buttons

--- js/towers/tower.js
+++ js/towers/tower.js
@@ -206,9 +206,9 @@ function showUpgradeInfo(){
     var tower = towers[selectedTower];
     if(tower) {
         if(tower.level < tower.maxUpgradeLevel) {
-            $("#towerLevel").text(tower.level + " --> " + (tower.level + 1));
-            $("#towerDamage").text(tower.damage + " --> " + Math.floor(tower.damage * 1.4));
-            $("#towerRange").text(tower.range + " --> " + Math.floor(tower.range * 1.125));
+            $("#towerLevel").text(tower.level + " ==> " + (tower.level + 1));
+            $("#towerDamage").text(tower.damage + " ==> " + Math.floor(tower.damage * 1.4));
+            $("#towerRange").text(tower.range + " ==> " + Math.floor(tower.range * 1.125));
         }
     }
 }
