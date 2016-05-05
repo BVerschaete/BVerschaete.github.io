efd878b120ad0dd5d51c9b5993ff51a4449051e8
BVerschaete <bastien.verschaete@gmail.com>
2016-05-05 16:52:34 +0200

    made tier 2 and 3 towers cost more

--- js/towers/tier2Tower.js
+++ js/towers/tier2Tower.js
@@ -10,6 +10,6 @@ Tower2.prototype.image = "tower2.png";
 Tower2.prototype.range = (game.tileSize*2.5); //100
 Tower2.prototype.fireRate = Tower.prototype.fireRate * 0.9;
 Tower2.prototype.damage = Tower.prototype.damage * 2;
-Tower2.prototype.cost = Tower.prototype.cost * 1.2;
+Tower2.prototype.cost = Tower.prototype.cost * 2.5;
 Tower2.prototype.maxUpgradeLevel = 6;
 Tower2.prototype.displayName = "Tower Tier 2";
--- js/towers/tier3Tower.js
+++ js/towers/tier3Tower.js
@@ -10,6 +10,6 @@ Tower3.prototype.image = "tower3.png";
 Tower3.prototype.range = (game.tileSize*3.125); //125
 Tower3.prototype.fireRate = Tower.prototype.fireRate * 0.8;
 Tower3.prototype.damage = Tower.prototype.damage * 3;
-Tower3.prototype.cost = Tower.prototype.cost * 1.4;
+Tower3.prototype.cost = Tower.prototype.cost * 3.5;
 Tower3.prototype.maxUpgradeLevel = 7;
 Tower3.prototype.displayName = "Tower Tier 3"; 
